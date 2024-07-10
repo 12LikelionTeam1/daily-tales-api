7054eff2b83a7fd31eadb67cc3fbf07f025a4f18
Gyumin Bae <bgmsound0528@gmail.com>
2024-07-10 11:46:13 +0900

    feat: 카카오 로그인 기능 추가

--- daily-tales-authentication/src/main/java/net/likelion/dailytales/authentication/infrastructure/gateway/KakaoOAuthGateway.java
+++ daily-tales-authentication/src/main/java/net/likelion/dailytales/authentication/infrastructure/gateway/KakaoOAuthGateway.java
@@ -1,11 +1,17 @@
 package net.likelion.dailytales.authentication.infrastructure.gateway;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.RequiredArgsConstructor;
 import net.likelion.dailytales.authentication.application.OAuthGateway;
 import net.likelion.dailytales.authentication.application.OAuthResource;
 import net.likelion.dailytales.authentication.application.OAuthType;
+import net.likelion.dailytales.core.domain.authentication.exception.AuthenticationFailedException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 
 @Component
 @RequiredArgsConstructor
@@ -13,9 +19,31 @@ public class KakaoOAuthGateway implements OAuthGateway {
     @Value("${oauth2.client.kakao.resource-uri}")
     private String resourceUri;
 
+    private final RestClient restClient;
+
     @Override
     public OAuthResource authenticate(String accessToken) {
-        return null;
+        KakaoResourceResponse resourceResponse = requestResource(accessToken);
+        return OAuthResource.of(OAuthType.KAKAO, resourceResponse.id());
+    }
+
+    private KakaoResourceResponse requestResource(String accessToken) {
+        ResponseEntity<KakaoResourceResponse> resource = restClient
+                .get()
+                .uri(resourceUri)
+                .headers(headers -> headers.setBearerAuth(accessToken))
+                .retrieve()
+                .onStatus(HttpStatusCode::isError, (request, response) -> {
+                    throw new AuthenticationFailedException();
+                })
+                .toEntity(KakaoResourceResponse.class);
+        return resource.getBody();
+    }
+
+    @JsonNaming(SnakeCaseStrategy.class)
+    private record KakaoResourceResponse(
+            String id
+    ) {
     }
 
     @Override
