1ac0be4003b76a707c38e3a7b4d93c17094bfea2
Gyumin Bae <bgmsound0528@gmail.com>
2024-07-21 20:04:17 +0900

    refactor: composite key 비교 메서드 오버라이딩

--- daily-tales-writing/src/main/java/net/likelion/dailytales/writing/infrastructure/persistence/entity/WritingCollectionEntityKey.java
+++ daily-tales-writing/src/main/java/net/likelion/dailytales/writing/infrastructure/persistence/entity/WritingCollectionEntityKey.java
@@ -10,4 +10,22 @@ import lombok.NoArgsConstructor;
 public class WritingCollectionEntityKey {
     private String userId = "";
     private String writingId = "";
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        WritingCollectionEntityKey that = (WritingCollectionEntityKey) o;
+
+        if (!userId.equals(that.userId)) return false;
+        return writingId.equals(that.writingId);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userId.hashCode();
+        result = 31 * result + writingId.hashCode();
+        return result;
+    }
 }
--- daily-tales-writing/src/main/java/net/likelion/dailytales/writing/infrastructure/persistence/entity/WritingKeywordEntityKey.java
+++ daily-tales-writing/src/main/java/net/likelion/dailytales/writing/infrastructure/persistence/entity/WritingKeywordEntityKey.java
@@ -10,4 +10,22 @@ import lombok.NoArgsConstructor;
 public class WritingKeywordEntityKey {
     private String writingId = "";
     private String keyword = "";
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        WritingKeywordEntityKey that = (WritingKeywordEntityKey) o;
+
+        if (!writingId.equals(that.writingId)) return false;
+        return keyword.equals(that.keyword);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = writingId.hashCode();
+        result = 31 * result + keyword.hashCode();
+        return result;
+    }
 }
